b5119f9fa1d3a631187d90b0c193711e88a3ebd4
SeayPrem <seayprem@hotmail.com>
2021-03-18 22:01:42 +0700

    ยังไม่เสร็จ

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+  openWeatherMap: {
+    BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECERT_KEY: "Your token"
+  }
+}
+
+module.exports = constants;
---
+++ utils/weatherData.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const constants = require('../config');
+
+const weatherData = (address, callback) => {
+  const url = constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECERT_KEY;
+  request({url, json:true}, (error, {body}) => {
+    if(error) {
+      callback("Can't fetch data from open weather map api ", undefined);
+    } else {
+      callback(undefined, {
+        temperature: body.main.temp,
+        description: body.weather[0].description,
+        cityName: body.name
+      })
+    }
+  });
+}
+
+module.exports = weatherData;
